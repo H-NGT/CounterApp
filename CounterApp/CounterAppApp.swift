711e7e45b4637fc9494430333d1894499f153b6e
H-NGT <nov.13.v3.0@gmail.com>
2022-07-24 22:20:56 +0900

    Initial Commit

---
+++ CounterApp/CounterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterAppApp.swift
+//  CounterApp
+//
+//  Created by 永田浩之 on 2022/07/24.
+//
+
+import SwiftUI
+
+@main
+struct CounterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
