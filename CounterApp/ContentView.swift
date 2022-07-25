bbefb5cfd578041785075cbe17dcfceb57c8275d
H-NGT <nov.13.v3.0@gmail.com>
2022-07-25 23:49:16 +0900

    カウンターアプリ演習完了

--- CounterApp/ContentView.swift
+++ CounterApp/ContentView.swift
@@ -11,13 +11,28 @@ struct ContentView: View {
     @State var number = 0
     var body: some View {
         VStack {
-            Text("\(number)")
-                .padding()
+            ZStack {
+                Image("counter")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Text("\(number)")
+                    .foregroundColor(.white)
+                    .font(/*@START_MENU_TOKEN@*/.largeTitle/*@END_MENU_TOKEN@*/)
+                    .padding()
+            }
             Button(action: {self.number += 1} ) {
                 Text("UP")
+                    .foregroundColor(.white)
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                    .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.blue/*@END_MENU_TOKEN@*/)
+                    .cornerRadius(12.0)
             }
             Button(action: {self.number -= 1} ) {
                 Text("DOWN")
+                    .foregroundColor(.white)
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                    .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.blue/*@END_MENU_TOKEN@*/)
+                    .cornerRadius(12.0)
             }
         }
     }
